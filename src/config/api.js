34b7adda52328f4fb52673ddb9245e2503b9be3e
Rizky Abdulah <90577776+bysofia@users.noreply.github.com>
2022-12-05 08:54:44 +0700

    update base url API

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-    baseURL: "http://localhost:5000/api/v1/",    
+    baseURL: "https://1985-2404-8000-1004-b94f-981-cb38-ac28-b410.ap.ngrok.io/api/v1/",    
 });
 
 export const setAuthToken = (token) => {
